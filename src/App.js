2f310651bb1805ae71594c534a842b285c4cedbc
Christian <christianyip92@gmail.com>
2022-11-09 13:16:43 -0800

    Redo portfolio section.
    Added title and description to each project item.
    
    Display is now one column.

--- src/App.js
+++ src/App.js
@@ -135,71 +135,179 @@ function App() {
         </p>
 
         <div className="portfolio">
-          <a
-            href="https://github.com/c-yip/shopping-cart"
-            className="portfolio__item"
-          >
-            <img
-              src={shoppingCartGif}
-              alt="gif of shopping cart app"
-              className="portfolio__img"
-            />
-          </a>
+          <div className="portfolio__item-container">
+            <div className="project-header-container">
+              <h3 className="project-header">Shopping Cart</h3>
+              <a
+                href="https://github.com/c-yip/shopping-cart"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <GitHubIcon fontSize="large" />
+              </a>
+            </div>
 
-          <a
-            href="https://github.com/c-yip/cv-application-generator"
-            className="portfolio__item"
-          >
-            <img
-              src={cvGif}
-              alt="gif of cv generator app"
-              className="portfolio__img"
-            />
-          </a>
+            <a
+              href="https://c-yip.github.io/shopping-cart/"
+              className="portfolio__item"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={shoppingCartGif}
+                alt="gif of shopping cart app"
+                className="portfolio__img"
+              />
+            </a>
+            <p className="project-description">
+              E-commerce website built with React that allows users to quickly
+              navigate pages, add and remove items to their cart, and filter
+              products by category
+            </p>
+          </div>
 
-          <a
-            href="https://github.com/c-yip/tv-trivia"
-            className="portfolio__item"
-          >
-            <img
-              src={tvGif}
-              alt="gif of TV trivia app"
-              className="portfolio__img"
-            />
-          </a>
+          <div className="portfolio__item-container">
+            <div className="project-header-container">
+              <h3 className="project-header">CV Generator</h3>
+              <a href="https://github.com/c-yip/cv-application-generator">
+                <GitHubIcon fontSize="large" />
+              </a>
+            </div>
+            <a
+              href="https://c-yip.github.io/cv-application-generator/"
+              className="portfolio__item"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={cvGif}
+                alt="gif of cv generator app"
+                className="portfolio__img"
+              />
+            </a>
+            <p className="project-description">
+              React application that generates a resume using a premade layout
+              and user input and saves the resume as a PDF
+            </p>
+          </div>
 
-          <a
-            href="https://github.com/c-yip/battleship"
-            className="portfolio__item"
-          >
-            <img
-              src={battleshipGif}
-              alt="gif of battleship app"
-              className="portfolio__img"
-            />
-          </a>
+          <div className="portfolio__item-container">
+            <div className="project-header-container">
+              <h3 className="project-header">TV Trivia</h3>
+              <a
+                href="https://github.com/c-yip/tv-trivia"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <GitHubIcon fontSize="large" />
+              </a>
+            </div>
 
-          <a
-            href="https://github.com/c-yip/weather-app"
-            className="portfolio__item"
-          >
-            <img
-              src={weatherGif}
-              alt="gif of weather app"
-              className="portfolio__img"
-            />
-          </a>
+            <a
+              href="https://c-yip.github.io/tv-trivia/"
+              className="portfolio__item"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={tvGif}
+                alt="gif of TV trivia app"
+                className="portfolio__img"
+              />
+            </a>
+            <p className="project-description">
+              TV Trivia React application that generates a set of multiple
+              choice questions for users and tracks user score
+            </p>
+          </div>
 
-          <a
-            href="https://github.com/c-yip/todo-list"
-            className="portfolio__item"
-          >
-            <img
-              src={todoGif}
-              alt="gif of todo list app"
-              className="portfolio__img"
-            />
-          </a>
+          <div className="portfolio__item-container">
+            <div className="project-header-container">
+              <h3 className="project-header">Battleship</h3>
+              <a
+                href="https://github.com/c-yip/battleship"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <GitHubIcon fontSize="large" />
+              </a>
+            </div>
+            <a
+              href="https://c-yip.github.io/battleship/"
+              className="portfolio__item"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={battleshipGif}
+                alt="gif of battleship app"
+                className="portfolio__img"
+              />
+            </a>
+            <p className="project-description">
+              Battleship game where a user plays against a computer and has
+              their score kept
+            </p>
+          </div>
+
+          <div className="portfolio__item-container">
+            <div className="project-header-container">
+              <h3 className="project-header">Weather App</h3>
+              <a
+                href="https://github.com/c-yip/weather-app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <GitHubIcon fontSize="large" />
+              </a>
+            </div>
+            <a
+              href="https://c-yip.github.io/weather-app/"
+              className="portfolio__item"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={weatherGif}
+                alt="gif of weather app"
+                className="portfolio__img"
+              />
+            </a>
+            <p className="project-description">
+              Utilized Open Weather API to display weather details and relevant
+              images by asynchronously fetching data
+            </p>
+          </div>
+
+          <div className="portfolio__item-container">
+            <div className="project-header-container">
+              <h3 className="project-header">Todo List</h3>
+              <a
+                href="https://github.com/c-yip/todo-list"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <GitHubIcon fontSize="large" />
+              </a>
+            </div>
+
+            <a
+              href="https://c-yip.github.io/todo-list/"
+              className="portfolio__item"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={todoGif}
+                alt="gif of todo list app"
+                className="portfolio__img"
+              />
+            </a>
+            <p className="project-description">
+              Todo list app that allows users to add, delete, and edit tasks and
+              organize them by project name
+            </p>
+          </div>
         </div>
       </section>
 
